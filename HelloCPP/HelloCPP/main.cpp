24cec13396cb6152cf2f2801904900371b4c8557
Harvey <1448616686@qq.com>
2014-07-01 17:37:17 +0800

    HelloCPP
    HelloCPP

---
+++ HelloCPP/HelloCPP/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloCPP
+//
+//  Created by Harvey on 14-7-1.
+//  Copyright (c) 2014年 iwise. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello, World!\n"<<endl;
+    return 0;
+}
+
